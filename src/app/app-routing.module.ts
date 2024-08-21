cba3179fd286e8c6e7bac4391dccec825314089f
David Rimar <rimardavid@gmail.com>
2024-08-21 13:21:13 +0200

    load about on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AboutPageComponent } from './pages/about/about-page/about-page.componen
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: AboutPageComponent
   },
   {
     path: 'blog/:id',
