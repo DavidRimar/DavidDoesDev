ea2db995ca77a7e29a4a6d3283a43e46043e96f3
David Rimar <rimardavid@gmail.com>
2025-03-25 11:00:25 +0100

    using ChangeDetectorRef

--- src/app/components/blog-post/blog-post.component.ts
+++ src/app/components/blog-post/blog-post.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, ElementRef, AfterViewChecked, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import * as Prism from 'prismjs';
+import { map, tap } from 'rxjs/operators';
+import Prism from 'prismjs';
 import 'prismjs/components/prism-csharp';
 import 'prismjs/themes/prism-okaidia.css';
 
@@ -16,18 +16,17 @@ import { BlogPostContent } from '../../models/blog-post-content.model';
   styleUrls: ['./blog-post.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class BlogPostComponent implements OnInit, AfterViewChecked {
+export class BlogPostComponent implements OnInit {
   @Input() urlHandle!: string;
 
   blogPost$: Observable<BlogPost> | undefined;
   blogPostContent$: Observable<SafeHtml> | undefined;
 
-  private highlighted = false;
-
   constructor(
     private contentfulService: ContentfulServiceService,
     private sanitizer: DomSanitizer,
-    private elRef: ElementRef
+    private elRef: ElementRef,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -40,20 +39,19 @@ export class BlogPostComponent implements OnInit, AfterViewChecked {
     this.blogPostContent$ = this.contentfulService.getContentByUrlHandle(this.urlHandle).pipe(
       map((entry: any) => {
         let content: string = entry || 'No content';
-        // Add a default language class to <code> tags if not present
+        // Preprocess: add language class if missing
         content = content.replace(/<code(?![^>]*class=")/g, '<code class="language-csharp"');
         return this.sanitizer.bypassSecurityTrustHtml(content);
+      }),
+      tap(() => {
+        // Force Angular to update view before highlighting
+        this.cdr.detectChanges();
+        // Delay highlighting slightly to ensure rendering is complete
+        setTimeout(() => {
+          Prism.highlightAll();
+          console.log('Prism.highlightAll() called');
+        }, 50);
       })
     );
-  }
-
-  ngAfterViewChecked(): void {
-    if (!this.highlighted) {
-      setTimeout(() => {
-        Prism.highlightAll();
-        this.highlighted = true;
-        console.log('Prism highlighting done');
-      }, 0);
-    }
   }
 }
